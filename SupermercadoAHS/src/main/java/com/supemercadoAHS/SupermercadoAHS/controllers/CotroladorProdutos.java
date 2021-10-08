58bb13622e3b9358dc1b39ebc68028394c599a05
andrehesantos <andrehsantos28@gmail.com>
2021-10-08 00:13:32 -0300

    Consulta de Produtos e adicionado Materialize(CSS)

--- SupermercadoAHS/src/main/java/com/supemercadoAHS/SupermercadoAHS/controllers/CotroladorProdutos.java
+++ SupermercadoAHS/src/main/java/com/supemercadoAHS/SupermercadoAHS/controllers/CotroladorProdutos.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.supemercadoAHS.SupermercadoAHS.models.Produto;
 import com.supemercadoAHS.SupermercadoAHS.repositories.ProdutoRepository;
@@ -23,6 +24,14 @@ public class CotroladorProdutos {
 	public String form(Produto prod) {
 		pr.save(prod);
 		return "redirect:/cadastrarProduto";
+	}
+	
+	@RequestMapping("/consultarProdutos")
+	public ModelAndView listaProdutos() {
+		ModelAndView mv = new ModelAndView("Produto/consultarProdutos");
+		Iterable<Produto> produtos = pr.findAll();
+		mv.addObject("produto", produtos);
+		return mv;
 	}
 		
 }
